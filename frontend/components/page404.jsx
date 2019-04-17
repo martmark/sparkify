4308b47be05b5b6ee414f977d9423aedf135ad1f
martmark <martinmarkaj@live.com>
2019-04-16 23:20:57 -0400

    Fixed 404

--- frontend/components/page404.jsx
+++ frontend/components/page404.jsx
@@ -8,6 +8,8 @@ class Page404 extends React.Component {
         <header className='signup-header'><Link to={'/'}>Sparkify</Link></header>
         <section className='page404-main'>
         <h2>404: Page Not Found</h2>
+        <br/>
+          <div style={{ textAlign: 'center', fontSize: '22px' }}><Link className='green-link' to={'/'}>Home</Link></div>
         </section>
       </div>
     );
