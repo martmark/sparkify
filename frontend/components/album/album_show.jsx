09b58d445d6ed47f0f2eac0fb8d191af13459294
martmark <martinmarkaj@live.com>
2019-12-06 13:07:26 -0500

    Add loading spinner

--- frontend/components/album/album_show.jsx
+++ frontend/components/album/album_show.jsx
@@ -54,13 +54,15 @@ class AlbumShow extends React.Component {
 
     if (loading) {
       return (
-        <div className='spinner-outer'>
-          <div class="spinner">
-            <div class="rect1"></div>
-            <div class="rect2"></div>
-            <div class="rect3"></div>
-            <div class="rect4"></div>
-            <div class="rect5"></div>
+        <div className='spinner-outer-2'>
+          <div className='spinner-outer'>
+            <div class="spinner">
+              <div class="rect1"></div>
+              <div class="rect2"></div>
+              <div class="rect3"></div>
+              <div class="rect4"></div>
+              <div class="rect5"></div>
+            </div>
           </div>
         </div>
       )
