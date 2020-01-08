eb7a1e1dbb68849bd9e8a9701f98b458f8ff4711
martmark <martinmarkaj@live.com>
2020-01-08 16:47:53 -0500

    Add contact links

--- frontend/components/main_content/settings.jsx
+++ frontend/components/main_content/settings.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import { IconContext } from "react-icons";
+import { IoLogoLinkedin, IoLogoGithub } from "react-icons/io";
 
 class Settings extends React.Component {
   constructor(props) {
@@ -16,6 +18,27 @@ class Settings extends React.Component {
             <p>
               Sparkify is a non-commercial clone of Spotify<br></br> built by Martin Markaj.
             </p>
+            <p>
+              React / Redux<br></br>
+              Ruby on Rails<br></br>
+              PostgreSQL
+            </p>
+          </div>
+          <div className='contact-icons'>
+            <IconContext.Provider
+              value={{ className: "reacticon", size: "3.4em" }}
+            >
+              <a href="https://www.linkedin.com/in/martinmarkaj" target="_blank">
+                <IoLogoLinkedin />
+              </a>
+            </IconContext.Provider>
+            <IconContext.Provider
+              value={{ className: "reacticon", size: "3em" }}
+            >
+              <a href="https://github.com/martmark" target="_blank">
+                <IoLogoGithub />
+              </a>
+            </IconContext.Provider>
           </div>
         </div>
       </div>
